2e7e1b36696905e23aa83960bf1a420e6a7a364f
Keerthanamurugesan2001 <keerthana.murugesan@softsuave.com>
2023-10-05 15:25:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LibCModule } from 'lib-c';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LibCModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+console.log(3)
